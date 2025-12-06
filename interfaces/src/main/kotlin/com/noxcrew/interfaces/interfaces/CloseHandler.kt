66f976bf99a5c9f7f06f0c6d387c4a3d1b815e46
StillLutto <still.lutto@gmail.com>
2025-12-06 17:08:45 +0100

    refactor: add invoke function to CloseHandler
    This is done for IDE tab complete purposes

--- interfaces/src/main/kotlin/com/noxcrew/interfaces/interfaces/CloseHandler.kt
+++ interfaces/src/main/kotlin/com/noxcrew/interfaces/interfaces/CloseHandler.kt
@@ -4,4 +4,6 @@ import com.noxcrew.interfaces.view.InterfaceView
 import org.bukkit.event.inventory.InventoryCloseEvent
 
 /** A handler run whenever a view is closed for a given reason. */
-public fun interface CloseHandler : suspend (InventoryCloseEvent.Reason, InterfaceView) -> Unit
+public fun interface CloseHandler {
+    public suspend operator fun invoke(reason: InventoryCloseEvent.Reason, view: InterfaceView)
+}
